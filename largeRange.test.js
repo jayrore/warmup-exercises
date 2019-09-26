9b00627bf1f0ecc844f2d93320c48ed9aa23b490
Jayro Salgado <jsalgado@nearsoft.com>
2019-09-25 17:24:19 -0700

    Refactored removed array space

--- largeRange.test.js
+++ largeRange.test.js
@@ -4,6 +4,7 @@ const test1 = [ 1, 2 ]
 const test2 = [8, 4, 2, 10, 3, 6, 7, 9, 1]
 
 test('Test #1',() => {
+  expect(largestRange([1])).toStrictEqual([1,1])
   expect(largestRange(test1)).toStrictEqual([1,2])
 })
 
--- largestRange.js
+++ largestRange.js
@@ -2,6 +2,7 @@ module.exports.largestRange = largestRange
 
 function largestRange(array) {
   let numbersMap = {}
+  let biggestRangeLength = 0
   let biggestRange = []
 
   for (let i = 0; i < array.length; i++) {
@@ -12,32 +13,43 @@ function largestRange(array) {
 
   for (let i = 0; i < array.length; i++) {
     const number = array[i]
-    const newRange = [ number ]
+    // const newRange = [ number ]
+    let left  = number - 1
+    let right = number + 1
+    let currentLength = 1
     // next loop if number is part of a range
     if (numbersMap[number] === true) {
       continue
     }
 
+    numbersMap[number] === true
+    // newRange.unshift(number)
+    // newRange.push(number)
+
     // << search to back
-    let firstNumber = number - 1
-    while (numbersMap.hasOwnProperty(firstNumber)) {
-      newRange.unshift(firstNumber)
-      firstNumber--
+    // let firstNumber = number - 1
+    // while ( firstNumber in numbersMap ) {
+    while ( left in numbersMap ) {
+      // newRange.unshift(firstNumber)
+      numbersMap[left] = true
+      left--
+      currentLength++
     }
 
     // >> search to front
-    let lastNumber  = number + 1
-    while (numbersMap.hasOwnProperty(lastNumber)) {
-      newRange.push(lastNumber)
-      lastNumber++
+    // let lastNumber  = number + 1
+    while ( right in numbersMap ) {
+      // newRange.push(lastNumber)
+      numbersMap[right] = true
+      right++
+      currentLength++
     }
 
-    numbersMap[number] === true
-
-    if (newRange.length > biggestRange.length) {
-      biggestRange = newRange
+    if (currentLength > biggestRangeLength) {
+      biggestRangeLength = currentLength
+      biggestRange = [ left + 1, right - 1 ]
     }
   }
 
-  return [biggestRange.shift(), biggestRange.pop() ]
+  return biggestRange
 }
