5f64acdf8a26b627837a52ab95e61ec91166bc21
Jayro Salgado <jsalgado@nearsoft.com>
2019-10-15 16:42:23 -0700

    twoNumbersSum.go

---
+++ go/twoNumbersSum.go
@@ -0,0 +1,26 @@
+package main
+import (
+  "fmt"
+  "sort"
+)
+
+func main(){
+  result := twoNumbersSum([]int{1,2,4,5,3},7)
+  fmt.Println(result)
+}
+
+func twoNumbersSum(array []int, targetSum int) []int{
+  sort.Ints(array)
+  left, right := 0, len(array)-1
+  for left < right {
+    currentSum := array[left] + array[right]
+    if currentSum == targetSum {
+      return []int{array[left], array[right]}
+    } else if currentSum < targetSum {
+      left += 1
+    } else {
+      right -= 1
+    }
+  }
+  return []int{}
+}
